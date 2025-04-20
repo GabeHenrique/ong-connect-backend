ccec0d89a9ad2605cf42989cf9faa98f8e344970
Gabriel Palermo <pallermmo2011@gmail.com>
2025-04-20 17:03:37 -0300

    Ajuste na URI de autenticação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,7 @@ import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { User } from "@prisma/client";
 
-@Controller("auth")
+@Controller("/api/auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
